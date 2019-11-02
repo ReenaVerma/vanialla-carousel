3c91efe585debfc1a7d1d0d5dca4a77bd37088f8
ReenaVerma <reena_verma@hotmail.com>
2019-11-02 20:23:12 +0000

    Tidy up app.js - make es6 friendly

--- app.js
+++ app.js
@@ -2,85 +2,32 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log('app.js loaded');
 
   const carouselContainer = document.getElementById('carousel-container');
-  const itemsContainer    = document.querySelector('.items-container');
   const previousButton    = document.getElementById('previous');
   const nextButton        = document.getElementById('next');
-  let currentImage        = 0;
   let count               = 0;
-  
-  
+
   console.log('count', count);
 
-  // nextButton.addEventListener("click", event => {
-  //   console.log('click');
-  //   ++count
-  //   // currentImage = (currentImage + 1) % count.length;
-  //   // document.getElementById("one").style.backgroundImage="url(' "+ arrayCarousel[currentImage]+ " ')";
-
-
-  // });
-
-
-  function slideImages(dir){
-    var totalChildren = carouselContainer.getElementsByTagName('div').length;
+  slideImages = (direction) => {
+    const totalChildren = carouselContainer.getElementsByTagName('div').length;
     console.log('totalChildren', totalChildren);
-    
-    // dir === "left" ? count ++ : count --;
 
-    if (dir === "left") {
-      count ++;
-    } else {
-      count --
-    }
-
-
+    direction === 'left' ? count ++ : count --;
     console.log('updated count', count);
 
     carouselContainer.classList.add('horizTranslate');
     carouselContainer.style.left = count * 200 + 'px';
-    
-    previousButton.style.display = count < 0 ? "block" : "none";
-    // Here, 4 is the number displayed at any given time
-    nextButton.style.display = count > 4 - totalChildren ? "block" : "none";
+    carouselContainer.classList.add("slideOutLeft")
+
+    previousButton.style.display = count < 0 ? 'block' : 'none';
+    nextButton.style.display = count > 4 - totalChildren ? 'block' : 'none';
   }
 
-  previousButton.addEventListener("click", function(e) {
-      slideImages("left");
-  });
-  nextButton.addEventListener("click", function(e) {
-      slideImages("right");
-      carouselContainer.classList.add("slideOutLeft")
+  previousButton.addEventListener('click', event => {
+      slideImages('left');
   });
 
-
-
-  // const arrayObjCarousel = [
-  //   {
-  //       id: 1,
-  //       image: 'https://www.fillmurray.com/g/200/200'
-  //   },
-  //   {
-  //       id: 2,
-  //       image: 'https://www.fillmurray.com/200/200'
-  //   },
-  //   {
-  //       id: 3,
-  //       image: 'https://www.fillmurray.com/200/200'
-  //   },
-  //   {
-  //       id: 4,
-  //       image: 'https://www.fillmurray.com/g/200/200'
-  //   },
-  //   {
-  //       id: 5,
-  //       image: 'https://www.fillmurray.com/200/100'
-  //   }
-  // ]
-
-  // const arrayCarousel = ['https://www.fillmurray.com/g/200/200', 'https://www.fillmurray.com/200/200', 'https://www.fillmurray.com/200/200', 'https://www.fillmurray.com/g/200/200', 'https://www.fillmurray.com/200/100'];
-
-  // console.log('arrayObjCarousel', arrayObjCarousel);
-  // console.log('arrayCarousel', arrayCarousel);
-
-
-}); 
+  nextButton.addEventListener('click', event => {
+      slideImages('right');
+  });
+});
